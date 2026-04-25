14da2b0fed52955cbee00e02bc2506ee7e690b25
Peter Sanders <peter@psanders.me>
2026-04-25 19:53:38 -0400

    chore: Configure lints

--- src/kuvo/cli.py
+++ src/kuvo/cli.py
@@ -1,4 +1,23 @@
-def main():
+# LICENSE HEADER MANAGED BY add-license-header
+#
+# Kuvo: reproducible OCI images for Python projects
+# Copyright (C) 2026  Peter Sanders
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+def main() -> None:
     print("Hello, world!")
 
 if __name__ == "__main__":
