04f6a14413bfa951499a035953cf744b63bd374f
Peter Sanders <peter@psanders.me>
2026-04-25 22:13:36 -0400

    feat: Generate OCI image layers
    Produce an image layer for the Python installation and the virtual
    environment, separately, so that the Python layer has better
    cache-friendliness.

--- src/kuvo/cli.py
+++ src/kuvo/cli.py
@@ -18,27 +18,66 @@
 #
 """The main command line program for this application."""
 
+import hashlib
 import pathlib
 import subprocess  # noqa: S404
+import tarfile
 import tempfile
 
 import click
 
+from kuvo import settings
+
 
 @click.group()
-def main() -> None:
+@click.pass_context
+def main(ctx: click.Context) -> None:
     """Reproducible OCI images for your Python projects."""
+    ctx.obj = settings.get_config()
     click.echo("Hello, world!")
 
 
 @main.command()
-def build() -> None:
+@click.pass_context
+def build(ctx: click.Context) -> None:
     """Build an OCI image for the current project."""
+    ctx.ensure_object(settings.Config)
+    out_path = pathlib.Path(ctx.obj.oci_path)
     click.echo("Running the build...")
     with tempfile.TemporaryDirectory() as tdstr:
         click.echo(f"Using temporary directory {tdstr}")
         td = pathlib.Path(tdstr)
         _install_package(td)
+
+        with tempfile.NamedTemporaryFile(
+            suffix=".tar", mode="wb", delete_on_close=False
+        ) as tmp:
+            path = pathlib.Path(tmp.name)
+
+            with tarfile.open(fileobj=tmp, mode="w") as tf:
+                _package_tar(tf, rootfs=td, include=["usr"])
+            tmp.close()
+
+            with path.open("rb") as hf:
+                digest = hashlib.file_digest(hf, hashlib.sha256).hexdigest()
+            target = out_path / f"blobs/sha256/{digest}"
+            target.parent.mkdir(parents=True, exist_ok=True)
+            path.copy(target)
+
+        with tempfile.NamedTemporaryFile(
+            suffix=".tar", mode="wb", delete_on_close=False
+        ) as tmp:
+            path = pathlib.Path(tmp.name)
+
+            with tarfile.open(fileobj=tmp, mode="w") as tf:
+                _package_tar(tf, rootfs=td, include=["app"])
+            tmp.close()
+
+            with path.open("rb") as hf:
+                digest = hashlib.file_digest(hf, hashlib.sha256).hexdigest()
+            target = out_path / f"blobs/sha256/{digest}"
+            target.parent.mkdir(parents=True, exist_ok=True)
+            path.copy(target)
 
 
 def _install_package(rootfs: pathlib.Path) -> None:
@@ -76,6 +115,29 @@ def _fix_shebangs(bin_dir: pathlib.Path, rootfs: pathlib.Path) -> None:
         if shebang.is_relative_to(rootfs):
             lines[0] = f"#!/{shebang.relative_to(rootfs)}"
             script.write_text("\n".join(lines) + "\n")
+
+
+def _package_tar(
+    tf: tarfile.TarFile, rootfs: pathlib.Path, include: list[str]
+) -> None:
+    paths: list[pathlib.Path] = []
+    for path in include:
+        paths += (rootfs / path).rglob("*")
+    paths.sort()
+
+    for path in paths:
+        ti = tf.gettarinfo(str(path), arcname=str(path.relative_to(rootfs)))
+        ti.uid = 0
+        ti.gid = 0
+        ti.uname = ""
+        ti.gname = ""
+        ti.mtime = 0
+
+        if path.is_file():
+            with pathlib.Path(path).open("rb") as f:
+                tf.addfile(ti, f)
+        else:
+            tf.addfile(ti)
 
 
 if __name__ == "__main__":
